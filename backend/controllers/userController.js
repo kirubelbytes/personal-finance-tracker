34162f340fbd525ac3b4dd99cb35e90b1af58b0d
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-08-06 10:32:04 -0700

    Implement changePassword logic with validation and hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import express from "express"
 import pool from "../libs/dabatase.js";
+import { comparePassword, hashPassword } from "../libs/index.js";
 export const getUser = async(req, res) => {
     try {
         const { userId } = req.body.user;
@@ -22,9 +23,43 @@ export const getUser = async(req, res) => {
     }
 }
 
-export const changePassword = () => {
+export const changePassword = async (req, res) => {
     try {
-        
+        const {userId } = req.body.user;
+        const { currentPassword , newPassword , confirmPassword } = req.body;
+        const userExist = await pool.query({
+            text : "SELECT * FROM tbluser WHERE id = $1",
+            values : [userId]
+        })
+        const user = userExist.rows[0];
+        if(!user) {
+          return res.status(404).json({
+                status : 'failed',
+                message : "user doesn't exist"
+            })
+        }
+        if(newPassword !== confirmPassword) {
+            return res.status(404).json({
+                status : 'failed',
+                message : "Password mismatch"
+            })
+        }
+        const isMatch = await comparePassword(currentPassword, user?.password )
+        if(!isMatch) {
+            return res.status(404).json({
+                status : 'failed',
+                message : "Password mismatch"
+            })
+        }
+        const hashedPassword = await hashPassword(newPassword);
+        await pool.query({
+            text : "UPDATE tbluser SET password = $1 WHERE id = $2",
+            values : [hashedPassword, userId]
+        })
+        res.status(200).json({
+            status : "Success",
+            message : "Password changed successfully"
+        })
     } catch (error) {
         console.log(error);
         res.status(500).json({status : "failed", message : error.message})
